72ef1dbd24a1aa0af38182cf7abba8070e2146f5
YuvalBar-or <yuvalb.mail@gmail.com>
2025-02-12 14:05:50 +0200

    Delete computer_coms5-main directory

--- computer_coms5-main/Matala-5-main/Labsetup/volumes/sniff-spoof.py
+++
@@ -1,9 +0,0 @@
-from sniffer import PacketSniffer
-if __name__ == "__main__":
-    s = PacketSniffer("icmp")
-    serverP = input("Enter the server port")
-    proxyP = input("Enter the proxy port")
-    s.setSP(serverP) # set to port here
-    s.setPP(proxyP) # set to port here
-    s.setFN("sniffer_snooper.txt")
-    s.sniff_packets()
--- computer_coms5-main/Matala-5-main/Labsetup/volumes/sniffing_passwords.py
+++
@@ -1,6 +0,0 @@
-from sniffer import PacketSniffer
-
-if __name__ == "__main__":
-    s_talnet = PacketSniffer("talnet")
-    s_talnet.setFN("telnet_passwords.txt")
-    s_talnet.sniff_packets()
